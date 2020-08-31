8a8b9f698f0b7e41a8ae2b05f9c8e3de1c21896d
melina <melinalps@gmail.com>
2020-08-30 22:06:25 -0300

    dataset-archive

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // localAPi: "http://untitled-ui.s3-website.us-east-2.amazonaws.com/"
+  // localAPi: "http://dataset-archive-ui.s3-website.us-east-2.amazonaws.com/"
   localAPi: "http://3.134.113.210:8080/"
 };
